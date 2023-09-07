780deb2c5783d53c55471a28fd5d85cfc5946c06
ZengQuanSheng <zengqs@mail.nankai.edu.cn>
2023-09-07 22:40:42 +0800

    add config for 4 GPUs

---
+++ projects/BEVFusion/configs/4gpu/bevfusion_lidar_voxel0075_second_secfpn_4xb8-cyclic-20e_nus-3d.py
@@ -0,0 +1,10 @@
+_base_ = ['../bevfusion_lidar_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d.py']
+
+
+train_dataloader = dict(
+    batch_size=8)
+auto_scale_lr = dict(enable=False, base_batch_size=32)
+
+vis_backends = [dict(type='LocalVisBackend'), dict(type='TensorboardVisBackend')]
+visualizer = dict(
+    type='Det3DLocalVisualizer', vis_backends=vis_backends, name='visualizer')
